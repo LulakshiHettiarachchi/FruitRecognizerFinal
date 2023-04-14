1324edac8fb3ca3828507adecedde32a4aeb70e9
Lulakshi Hettiarachchi <lulakshinadunihettiarachchi@gmail.com>
2023-04-14 20:43:44 +0530

    first commit

---
+++ lib/styles.dart
@@ -0,0 +1,75 @@
+// Copyright (c) 2022 Kodeco LLC
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+
+// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+// distribute, sublicense, create a derivative work, and/or sell copies of the
+// Software in any work that is designed, intended, or marketed for pedagogical
+// or instructional purposes related to programming, coding,
+// application development, or information technology.  Permission for such use,
+// copying, modification, merger, publication, distribution, sublicensing,
+// creation of derivative works, or sale is expressly withheld.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+import 'package:flutter/material.dart';
+
+const kMainFont = 'Roboto';
+const kButtonFont = 'Roboto';
+const kDisplayFont = 'SquadaOne';
+
+const kColorGreen = Color.fromARGB(255, 20, 61, 91);
+const kColorLightGreen = Color.fromARGB(255, 50, 161, 212);
+const kColorBrown = Color.fromARGB(255, 223, 87, 38);
+const kColorLightYellow = Color.fromARGB(255, 220, 204, 112);
+
+const kColorRed = Color.fromARGB(255, 186, 45, 45);
+const kColorLightRed = Color(0xFFF2CECE);
+const kColorLightGray = Color(0xFFDDDDDD);
+
+const kColorHunterGreen = Color.fromARGB(255, 82, 212, 238);
+const kColorMayGreen = Color.fromARGB(255, 34, 79, 8);
+const kColorAndroidGreen = Color.fromARGB(255, 49, 225, 181);
+const kColorEggshell = Color.fromARGB(255, 14, 12, 9);
+const kColorBitterSweetShimmer = Color.fromARGB(255, 141, 18, 20);
+
+const kBgColor = kColorGreen;
+
+const kTitleTextStyle = TextStyle(
+  fontFamily: kDisplayFont,
+  fontSize: 50.0,
+  color: kColorAndroidGreen,
+  decoration: TextDecoration.none,
+);
+
+const kAnalyzingTextStyle = TextStyle(
+    fontFamily: kMainFont,
+    fontSize: 25.0,
+    color: kColorEggshell,
+    decoration: TextDecoration.none);
+
+const kResultTextStyle = TextStyle(
+    fontFamily: kDisplayFont,
+    fontSize: 35.0,
+    color: kColorLightYellow,
+    decoration: TextDecoration.none);
+
+const kResultRatingTextStyle = TextStyle(
+    fontFamily: kMainFont,
+    fontSize: 18.0,
+    color: Colors.white,
+    decoration: TextDecoration.none);
